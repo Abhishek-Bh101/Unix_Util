cde1df47039d122f045889268a87bd24a8bf5ae6
Abhishek-Bh101 <abhishek.b48723@gmail.com>
2026-02-10 20:00:45 +0530

    Add return 0 to main and fix % output when fp is NULL

--- wcat/wcat.c
+++ wcat/wcat.c
@@ -5,12 +5,13 @@
 int main(int argc, char* argv[]) {	
 	FILE *fp = fopen(argv[1], "r");
 	if(fp == NULL){
-		printf("The file does not exist");
+		printf("The file does not exist\n");
 		return(1);
 	}
 	char buffer[100];
 	while(fgets(buffer, sizeof buffer, fp) != NULL){
 		printf("%s", buffer);
 	}
+	return 0;
 }
 
